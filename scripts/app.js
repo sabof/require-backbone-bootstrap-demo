f496ab4b9b04c389fca6284eff72d2dd33feb6a7
sabof <esabof@gmail.com>
2014-04-02 14:21:43 +0100

    WIP

--- scripts/app.js
+++ scripts/app.js
@@ -7,29 +7,32 @@ define(function(require) {
 
   var ItemTemplate = require('text!../templates/title-item.html');
 
-  var Title = Backbone.Model.extend({
+  var TitleModel = Backbone.Model.extend({
     defaults: function() {
       return {
 	      description: null,
 	      name: null,
 	      id: null
       };
+    },
 
+    sync: function(method, model, options) {
+      if (method == 'delete') {
+        // return $.ajax('');
+      } else {
+        return Backbone.sync(method, model, options);
+      }
     }
   });
 
-  var AvailableTitles = Backbone.Collection.extend({
+  var AvailableTitlesModel = Backbone.Collection.extend({
     url: 'http://217.18.25.29:10070/gametitles/list',
 
-    model: Title,
+    model: TitleModel,
 
     parse: function(response, options) {
       return response.titles;
     }
-
-    // sync: function(method, model) {
-    //   // method can only be 'read'
-    // }
   });
 
   var User = Backbone.Model.extend({
@@ -76,6 +79,27 @@ define(function(require) {
 
     template: _.template(ItemTemplate),
 
+    deleteTitle: function(event) {
+      // console.log('deleted', this);
+      this.model.destroy();
+    },
+
+    initialize: function() {
+      var self = this;
+
+      this.model.on('remove', function() {
+        self.remove();
+      });
+
+      // this.model.on("all", function(eventName){
+      //   console.log(eventName + ' was triggered!');
+      // });
+    },
+
+    events:{
+      "click .delete": "deleteTitle"
+    },
+
     render: function() {
       this.$el.html(
         this.template(
@@ -85,24 +109,37 @@ define(function(require) {
     }
   });
 
-  var TitlesView = Backbone.View.extend({
+  var UserTitlesView = Backbone.View.extend({
+
+  });
+
+  var AvailableTitlesView = Backbone.View.extend({
     tagName: 'ul',
-    initialize:function () {
+
+    initialize: function () {
+      // this.model.bind("reset", this.render, this);
+      // var self = this;
+      // this.model.bind("add", function (wine) {
+      //   $(self.el).append(new WineListItemView({model:wine}).render().el);
+      // });
+
       // this.model.bind("reset", this.render, this);
       // this.model.bind("change", this.render, this);
+
+      // FIXME: Change to add?
       this.model.bind("sync", this.render, this);
 
-      this.model.on("all", function(eventName){
-        console.log(eventName + ' was triggered!');
-      });
+      // this.model.on("all", function(eventName){
+      //   console.log(eventName + ' was triggered!');
+      // });
     },
 
     render:function (eventName) {
-      console.log('length', this.model.models.length);
+      // console.log('length', this.model.models.length);
       _.each(this.model.models, function (title) {
-        console.log('adding', title);
+        // console.log('adding', title);
 
-        $(this.el).append(
+        this.$el.append(
           new TitleView({model: title})
             .render()
             .el
@@ -110,20 +147,53 @@ define(function(require) {
       }, this);
 
       return this;
-    }
+    },
+
   });
 
   var SignInPage = Backbone.View.extend({});
 
-  // var item = new TitleView({
-
-  // });
-
   // $('#main-content').html(item.render().el);
-  var list = window.list = new TitlesView({
-    model: new AvailableTitles()
+
+  var app = new App();
+
+  var list = window.list = new AvailableTitlesView({
+    app: app,
+
+    model: new AvailableTitlesModel({
+      app: app
+    })
   });
-  $('#main-content').html(list.render().el);
+
+  $('#pages .sign-in').html(list.render().el);
   list.model.fetch();
 
 });
+
+// // Router
+// var AppRouter = Backbone.Router.extend({
+
+//     routes:{
+//         "":"list",
+//         "wines/:id":"wineDetails"
+//     },
+
+//     initialize:function () {
+//         $('#header').html(new HeaderView().render().el);
+//     },
+
+//     list:function () {
+//         this.wineList = new WineCollection();
+//         this.wineListView = new WineListView({model:this.wineList});
+//         this.wineList.fetch();
+//         $('#sidebar').html(this.wineListView.render().el);
+//     },
+
+//     wineDetails:function (id) {
+//         this.wine = this.wineList.get(id);
+//         if (app.wineView) app.wineView.close();
+//         this.wineView = new WineView({model:this.wine});
+//         $('#content').html(this.wineView.render().el);
+//     }
+
+// });
